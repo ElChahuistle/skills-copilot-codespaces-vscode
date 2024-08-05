ac4f6a98969ae3dc1e8c6d87f7cd590b37f1cb45
Gerardo Thomas <ElChahuistle@users.noreply.github.com>
2024-08-05 22:25:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,45 +1,25 @@
-// Create new webserver
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Create web server
+// 1. Import express
+// 2. Create a new express instance
+// 3. Create a new route
+// 4. Create a new route for comments
+// 5. Export the express instance
 
-// Create new database
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/comments');
+// 1. Import express
+const express = require('express');
 
-// Create new comment schema
-var commentSchema = new mongoose.Schema({
-  name: String,
-  comment: String
+// 2. Create a new express instance
+const app = express();
+
+// 3. Create a new route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-// Create new comment model
-var Comment = mongoose.model('Comment', commentSchema);
-
-// Serve static files
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+// 4. Create a new route for comments
+app.get('/comments', (req, res) => {
+  res.send('This is a comments route');
 });
 
-// Create new comment
-app.post('/comment', function(req, res) {
-  var comment = new Comment(req.body);
-  comment.save(function(err) {
-    if (err) console.log(err);
-    io.emit('comment', req.body);
-    res.json({ status: 'ok' });
-  });
-});
-
-// Get all comments
-app.get('/comments', function(req, res) {
-  Comment.find({}, function(err, comments) {
-    if (err) console.log(err);
-    res.json(comments);
-  });
-});
-
-// Start webserver
-http.listen(3000, function() {
-  console.log('listening on *:3000');
-});
+// 5. Export the express instance
+module.exports = app;
